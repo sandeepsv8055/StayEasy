c25817d1848ff5d3d11c04f89ffa99c04f3c7a6e
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2025-08-06 01:03:40 +0530

    1
    basics

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,16 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#F5385D'
+      },
+    },
   },
   plugins: [],
 }
+
