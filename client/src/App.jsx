1107e2eaf0402408192525fe94e39472b01b47af
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2025-08-05 13:28:38 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 
+
 function App() {
   
 
   return (
-    <div>Hello</div>
-  )
+    <div className="text-3xl font-bold text-blue-600">
+      Tailwinf is working
+    </div>
+  );
 }
 
 export default App
